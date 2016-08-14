c9f7d8bbf4fbafb7d4d4f6a3c021b60f2acd3aa6
irvin <irvin.vivanco@manya.pe>
2016-08-14 18:13:25 -0500

    ..

--- desarrollo/js/myapp/myapp.js
+++ desarrollo/js/myapp/myapp.js
@@ -290,10 +290,10 @@ $(document).on('ready',function() {
 
 
     // Datepicker
-    /*$('.datepicker').pickadate({
+    $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
-    });*/
+    });
 
     // Collapse
     $('.collapsible').collapsible({
