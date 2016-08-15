f1ea1306c62beadb4af007d7612fddd26547e5db
Irvin La Maravilla <irvin.manya@manya.pe>
2016-08-15 10:28:29 -0500

    ..

--- templates/index.php
+++ templates/index.php
@@ -117,63 +117,37 @@
 				<div class="titleBox2">
 					<h2>Noticias destacadas</h2>	
 				</div>
-				<article class="col l4 m12 s12">
-					<div class="notItemHome itemShadow margBot20">
-						<figure>
-							<a href="javascript:void(0)" title="">
-								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="">
-							</a>
-							<figcaption>
-								<h3>
-									<a href="javascript:void(0)" title="">
-										John Boyega en el reparto de "Pacific Rim 2"
-									</a>
-								</h3>
-								<p>
-									El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
-								</p>
-							</figcaption>
-						</figure>
-					</div>
-				</article>
-				<article class="col l4 m12 s12">
-					<div class="notItemHome itemShadow margBot20">
-						<figure>
-							<a href="javascript:void(0)" title="">
-								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="">
-							</a>
-							<figcaption>
-								<h3>
-									<a href="javascript:void(0)" title="">
-										John Boyega en el reparto de "Pacific Rim 2"
-									</a>
-								</h3>
-								<p>
-									El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
-								</p>
-							</figcaption>
-						</figure>
-					</div>
-				</article>
-				<article class="col l4 m12 s12">
-					<div class="notItemHome itemShadow margBot20">
-						<figure>
-							<a href="javascript:void(0)" title="">
-								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="">
-							</a>
-							<figcaption>
-								<h3>
-									<a href="javascript:void(0)" title="">
-										John Boyega en el reparto de "Pacific Rim 2"
-									</a>
-								</h3>
-								<p>
-									El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
-								</p>
-							</figcaption>
-						</figure>
-					</div>
-				</article>
+
+				<?php $args = array(
+					'posts_per_page' => '1',
+					'cat' => 227
+				);?>
+				<?php $the_query = new WP_Query($args); ?>
+					<?php if ($the_query->have_posts()) : ?>
+						<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+							<article class="col l4 m12 s12">
+								<div class="notItemHome itemShadow margBot20">
+									<figure>
+										<a href="javascript:void(0)" title="">
+											<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="">
+										</a>
+										<figcaption>
+											<h3>
+												<a href="javascript:void(0)" title="">
+													John Boyega en el reparto de "Pacific Rim 2"
+												</a>
+											</h3>
+											<p>
+												El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
+											</p>
+										</figcaption>
+									</figure>
+								</div>
+							</article>
+						<?php endwhile; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
+
 				<a href="javascript:void(0)" class="btnPlusAbs" title="">
 					<i>
 						<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/icoPlus.svg" alt="title" title="">
