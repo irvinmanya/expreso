8e990e13c9af064f12bea7bfcbfd65c0a6cd4787
irvin <irvin.vivanco@manya.pe>
2016-08-14 19:07:17 -0500

    ..

--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@
 <?php get_header(); ?>
 
 <?php //Publicidad - Long ?>
-<?php include('../content/publong.php'); ?>
+<?php include('../../content/publong.php'); ?>
 
 <?php //Intro ?>
 <section class="secCont intro">
--- templates/publicidad.php
+++ templates/publicidad.php
@@ -6,7 +6,7 @@
 <?php get_header(); ?>
 
 <?php //Publicidad - Long ?>
-<?php include('../content/publong.php'); ?>fin
+<?php include('../content/publong.php'); ?>
 
 <?php if( have_rows('pub-cont') ): ?>
 	<?php while ( have_rows('pub-cont') ) : the_row(); ?>
