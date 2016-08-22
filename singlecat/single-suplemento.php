5a889bba82bc67203f16fa121d0e1e3428785a61
irvin <irvin.vivanco@manya.pe>
2016-08-22 12:47:09 -0500

    ..

---
+++ singlecat/single-suplemento.php
@@ -0,0 +1,221 @@
+<?php //Interior de suplementos ?>
+<section class="secrow single">
+	<div class="container">
+		<div class="row">
+			<div class="col l8 m12 s12 lineLateralRight" style="position:relative;">
+
+				<?php //Titulo ?>
+				<div class="titleBox1">
+					<h1>
+						<?php the_title() ?>
+					</h1>
+				</div>
+
+				<?php if (false) { ?>
+					<div class="singSlider owlOneItem">
+						<div class="singItem">
+							<figure>
+								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+							</figure>
+						</div>
+						<div class="singItem">
+							<figure>
+								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+							</figure>
+						</div>
+						<div class="singItem">
+							<figure>
+								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+							</figure>
+						</div>
+					</div>
+				<?php } ?>
+
+				<div class="singItem">
+					<figure>
+						<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+					</figure>
+				</div>
+
+				<?php //Contenido ?>
+				<div class="campTxt lineBottom">
+					<?php the_content(); ?>
+				</div>
+
+				
+				<?php //Articulos relacionados ?>
+				<div class="rowSing rowRell lineBottom">
+					<div class="titleBox2">
+						<h2>
+							Suplementos relacionadas
+						</h2>
+					</div>
+					<article class="col l4 m12 s12">
+						<div class="suplItem itemShadow margBot20">
+							<figure>
+								<a href="" title="">
+									<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/bloguero.png" alt="" title="">
+								</a>
+							</figure>
+							<div class="suplTxt">
+								<h3>
+									<a href="" title="" >
+										Titulo del suplemento
+									</a>
+								</h3>
+							</div>
+						</div>
+					</article>
+					<article class="col l4 m12 s12">
+						<div class="suplItem itemShadow margBot20">
+							<figure>
+								<a href="" title="">
+									<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/bloguero.png" alt="" title="">
+								</a>
+							</figure>
+							<div class="suplTxt">
+								<h3>
+									<a href="" title="" >
+										Titulo del suplemento
+									</a>
+								</h3>
+							</div>
+						</div>
+					</article>
+					<article class="col l4 m12 s12">
+						<div class="suplItem itemShadow margBot20">
+							<figure>
+								<a href="" title="">
+									<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/bloguero.png" alt="" title="">
+								</a>
+							</figure>
+							<div class="suplTxt">
+								<h3>
+									<a href="" title="" >
+										Titulo del suplemento
+									</a>
+								</h3>
+							</div>
+						</div>
+					</article>
+				</div>
+
+			</div>
+
+			<?php //Sidebar ?>
+			<div class="col l4 m12 s12">
+
+				<?php //Publicidad ?>				
+				<?php get_template_part( 'content/content', 'pubsmall' ); ?>
+				
+				<?php //Entrvista ?>
+				<article class="itemLast itemShadow margBot20">
+					<figure>
+						<figcaption>
+							<h3>
+								Entrevista
+							</h3>
+						</figcaption>
+						<a href="javascript:void(0)" title="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+						</a>
+						<figcaption>
+							<p>
+								El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
+							</p>
+						</figcaption>
+					</figure>
+				</article>
+
+				<?php //Entrevista ?>
+				<article class="itemLast itemShadow margBot20">
+					<figure>
+						<figcaption>
+							<h3>
+								Entrevista
+							</h3>
+						</figcaption>
+						<a href="javascript:void(0)" title="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+						</a>
+						<figcaption>
+							<p>
+								El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
+							</p>
+						</figcaption>
+					</figure>
+				</article>
+
+				<?php //Destacada del día  ?>
+				<article class="itemLast itemShadow margBot20">
+					<figure>
+						<figcaption>
+							<h3>
+								Destacada del día
+							</h3>
+						</figcaption>
+						<a href="javascript:void(0)" title="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+						</a>
+						<figcaption>
+							<h4>
+								<a href="javascript:void(0)" title="">
+									Jhon Boyega en el reparto de "Pacific Rim 2"
+								</a>
+							</h4>
+							<p>
+								El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
+							</p>
+						</figcaption>
+					</figure>
+				</article>
+
+				<?php //Encuesta ?>
+				<div class="encBox itemShadow margBot20">
+					<p>
+						¿Considera exagerado el tiempo que se está tomando la ONPE para dar los resultados electorales al 100%?
+					</p>
+					<div class="encInpCont">
+						<div class="col l6 m6 s6">
+							<p>
+								<input id="rdoSi" name="group1" type="radio" />
+								<label for="rdoSi">Si</label>
+							</p>
+						</div>
+						<div class="col l6 m6 s6">
+							<p>
+								<input id="rdoNo" name="group1" type="radio"/>
+								<label for="rdoNo">No</label>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<?php //Publicidad ?>
+				<?php get_template_part( 'content/content', 'pubsmall' ); ?>
+
+				<?php //Suplementos del día ?>
+				<article class="itemLast itemShadow margBot20">
+					<figure>
+						<figcaption>
+							<h3>
+								Suplementos del dia
+							</h3>
+						</figcaption>
+						<a href="javascript:void(0)" title="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="title">
+						</a>
+						<figcaption>
+							<p>
+								<a href="javascript:void(0)" title="">
+									El actor visto en "Attack The Block" o en el Episodio VII de 'La guerra de...
+								</a>
+							</p>
+						</figcaption>
+					</figure>
+				</article>
+
+			</div>
+		</div>
+	</div>
+</section>
