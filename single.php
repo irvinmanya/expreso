3a0d3bff0f4d6dc6e1db2821ef11690fca64f368
irvin <irvin.vivanco@manya.pe>
2016-08-22 10:53:49 -0500

    ..

--- single.php
+++ single.php
@@ -11,12 +11,12 @@ if (in_category('editoriales')) {
 }elseif(in_category('galeria-de-portadas')){
 	get_template_part( 'singlecat/single', 'portadas' );
 }elseif(in_category('blogueros')){
-	$subcategories = get_categories('&child_of=2&hide_empty=1'); 
+	$subcategories = get_categories('child_of=2,hide_empty=1'); 
 	foreach ($subcategories as $subcategory) {
 		get_template_part( 'singlecat/single', 'blogueros' );
 	}
 }elseif(in_category('opinion')){
-	$subcategories = get_categories('&child_of=2&hide_empty=1'); 
+	$subcategories = get_categories('child_of=2,hide_empty=1'); 
 	foreach ($subcategories as $subcategory) {
 		get_template_part( 'singlecat/single', 'blogueros' );
 	}
