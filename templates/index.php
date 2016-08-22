ed888079b7cf9968b5a62d8f2fbf32f74f3f5269
irvin <irvin.vivanco@manya.pe>
2016-08-22 17:56:00 -0500

    ..

--- templates/index.php
+++ templates/index.php
@@ -85,7 +85,7 @@
 									<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 										<div class="itemLast itemShadow">
 											<figure>
-												<figcaption class="pDayTitle">
+												<figcaption>
 													<h3>
 														Portada del día
 													</h3>
