a05d4b7462d4169ba35bae22d74414455db9cb5a
irvin <irvin.vivanco@manya.pe>
2016-08-22 23:20:03 -0500

    ..

--- subcat_blogcolum.php
+++ subcat_blogcolum.php
@@ -4,7 +4,6 @@
 		<div class="row">
 			<?php //filtros ?>
 			<div class="col l3 m12 s12 rowFiltros lineLateralRight">
-
 				<div class="blogAutor itemShadow lineBottom">
 					<figure>
 						<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/noticia.jpg" alt="title" title="">
@@ -33,40 +32,47 @@
 						</span>
 					</div>
 				<?php } ?>
-
 				<?php //Publicidad - Small ?>
 				<?php get_template_part( 'content/content', 'pubsmall' ); ?>
 			</div>
 
 			<?php //Content ?>
 			<div class="col l9 m12 s12">
-				<article class="col l6 m12 s12">
-					<div class="blogerItem itemShadow margBot20">
-						<figure>
-							<a href="<?php echo esc_url( $category_link ); ?>" title="<?php echo $category->name; ?>" >
-								<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/bloguero.png" alt="<?php echo $category->name; ?>" title="<?php echo $category->name; ?>">
-							</a>
-						</figure>
-						<div class="blogerTxt">
-							<h3>
-								<a href="" title="" >
-									Titulo
-								</a>
-							</h3>
-							<?php if (false) { ?>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-								</p>
-							<?php } ?>
-							<a href="<?php echo esc_url( $category_link ); ?>" title="<?php echo $category->name; ?>"  class="linkPlus">
-								Míralo ahora
-								<i>
-									<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/arrowLeftMod.svg" alt="" title="">
-								</i>
-							</a>
-						</div>
-					</div>
-				</article>
+				<?php $args = array(
+					'posts_per_page' => '1',
+					'cat' => get_query_var('cat')
+				);?>
+				<?php $the_query = new WP_Query($args); ?>
+					<?php if ($the_query->have_posts()) : ?>
+						<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+							<article class="col l6 m12 s12">
+								<div class="blogerItem itemShadow margBot20">
+									<figure>
+										<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+											<?php the_post_thumbnail(); ?>
+										</a>
+									</figure>
+									<div class="blogerTxt">
+										<h3>
+											<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+												<?php the_title(); ?>
+											</a>
+										</h3>
+										<p>
+											<?php the_excerpt(); ?>
+										</p>
+										<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="linkPlus">
+											Míralo ahora
+											<i>
+												<img src="<?php echo get_template_directory_uri() ?>/img/plantilla/arrowLeftMod.svg" alt="" title="">
+											</i>
+										</a>
+									</div>
+								</div>
+							</article>
+						<?php endwhile; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
 			</div>
 
 		</div>
