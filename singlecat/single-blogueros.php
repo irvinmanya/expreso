3789d9499675b75f4f0a810f2937b041f4dc44cd
irvin <irvin>
2016-08-24 11:11:22 -0500

    ..

--- singlecat/single-blogueros.php
+++ singlecat/single-blogueros.php
@@ -44,33 +44,34 @@
 					</span>
 				</div>
 
-				<div class="titleBox3">
-					<h3>
-						Filtra por tema
-					</h3>
-				</div>
-				<div class="filtroBox lineBottom">
-					<p>
-						<input type="checkbox" id="tema1" name="tema1"/>
-						<label for="tema1">Tema #1</label>
-					</p>
-					<p>
-						<input type="checkbox" id="tema2" name="tema2"/>
-						<label for="tema2">Tema #2</label>
-					</p>
-					<p>
-						<input type="checkbox" id="tema3" name="tema3"/>
-						<label for="tema3">Tema #3</label>
-					</p>
-					<p>
-						<input type="checkbox" id="tema4" name="tema4"/>
-						<label for="tema4">Tema #4</label>
-					</p>
-					<div class="inpFiltro">
-						<input type="date" name="fecEdit" id="fecEdit" class="datepicker picker__input" placeholder="Escoja una fecha">
+				<?php if (false) { ?>
+					<div class="titleBox3">
+						<h3>
+							Filtra por tema
+						</h3>
 					</div>
-				</div>
-
+					<div class="filtroBox lineBottom">
+						<p>
+							<input type="checkbox" id="tema1" name="tema1"/>
+							<label for="tema1">Tema #1</label>
+						</p>
+						<p>
+							<input type="checkbox" id="tema2" name="tema2"/>
+							<label for="tema2">Tema #2</label>
+						</p>
+						<p>
+							<input type="checkbox" id="tema3" name="tema3"/>
+							<label for="tema3">Tema #3</label>
+						</p>
+						<p>
+							<input type="checkbox" id="tema4" name="tema4"/>
+							<label for="tema4">Tema #4</label>
+						</p>
+						<div class="inpFiltro">
+							<input type="date" name="fecEdit" id="fecEdit" class="datepicker picker__input" placeholder="Escoja una fecha">
+						</div>
+					</div>
+				<?php } ?>
 
 				<?php //Publicidad - Small ?>
 				<?php get_template_part( 'content/content', 'pubsmall' ); ?>
